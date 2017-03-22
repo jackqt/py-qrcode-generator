22e1207044f3210dd46268bb88dfa7e8789f4e18
Mac <mac@Jacks-MacBook.local>
2017-03-22 19:25:09 +0800

    Return qrcode image path directly

--- app/QRCodeConverter.py
+++ app/QRCodeConverter.py
@@ -4,6 +4,7 @@
 import cStringIO
 import urllib
 import qrcode
+import random
 from PIL import Image
 from util import Util
 from log import Logger
@@ -12,8 +13,8 @@ class QRCodeConverter:
     def __init__(self):
         self.util = Util()
         self.log = Logger()
-        self.block_size = 8
-        self.circle_radius = self.block_size * 5
+        self.box_size = 10
+        self.border = 4
         self.qrcode_img_name_prefix = "qrcode"
         self.qrcode_img_name_surffix = ".png"
 
@@ -26,7 +27,7 @@ class QRCodeConverter:
     def set_qrcode_size(self, size):
         self.size = size
 
-    def get_qrcode_thumbnail_size(self):
+    def get_qrcode_size(self):
         assert self.size
         assert isinstance(self.size, tuple)
         assert len(self.size) == 2
@@ -45,9 +46,22 @@ class QRCodeConverter:
             f = cStringIO.StringIO(path)
         return f
 
-    def process(self, successCallback, failureCallback):
-        im = qrcode.make(self.content).convert('RGBA')
-        outputname = self.qrcode_img_name_prefix + str(self.util.now()) + self.qrcode_img_name_surffix
+    def make_qrcode_img(self, content):
+        qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=self.box_size,
+                border=self.border,
+        )
+        qr.add_data(content)
+        qr.make(fit=True)
+
+        img = qr.make_image()
+        return img
+
+    def process(self):
+        im = self.make_qrcode_img(self.content).convert('RGBA')
+        outputname = self.qrcode_img_name_prefix + str(self.util.now()) + str(int(random.random()*100000)) + self.qrcode_img_name_surffix
 
         try:
             inner_img_file = self.fetch_file(self.inner_img_path)
@@ -55,9 +69,7 @@ class QRCodeConverter:
 
             (width, height) = inner_img.size
 
-            dim = height
-            if (width > dim):
-                dim = width
+            dim = height if width <= height else width
 
             xTrans = (im.size[0] - width) / 2.0
             yTrans = (im.size[1] - dim) / 2.0
@@ -65,17 +77,19 @@ class QRCodeConverter:
             im.paste(inner_img, (int(xTrans), int(yTrans)), inner_img)
 
             try:
-                (img_width, img_height) = get_qrcode_thumbnail_size()
+                (img_width, img_height) = self.get_qrcode_size()
                 self.log.debug('Get width and height for qrcode image')
-                self.log.debug('Set thunbnail for qrcode with size: (%d, %d)' % (img_width, img_height))
-                im.thumbnail(self.size)
-            except Exception as thumbnail_exception:
+                self.log.debug('Set thumbnail for qrcode with size: (%d, %d)' % (img_width, img_height))
+                im = im.resize(self.size)
+            except Exception as resize_exception:
                 self.log.debug('Cannot get suitable width and height for qrcode image, do nothing')
+                self.log.warn(resize_exception)
         except Exception as e:
             self.log.warn('Cannot fetch inner image with path: %s' %(self.inner_img_path))
             self.log.warn(e)
 
         im.save(outputname)
 
-        self.log.info("success to process, now call successCallback, qrcode file: " + outputname)
-        successCallback(self)
+        self.qrcode_path = outputname
+        self.log.debug("success to process, generated qrcode file: " + outputname)
+        return self.qrcode_path
